714220792298d2987679ad21ce3c91acd1695e53
mihajlo182savic <savic.ra182.2019@uns.ac.rs>
2023-04-01 12:32:02 +0200

    Interceptor

--- src/app/service/login-register.service.ts
+++ src/app/service/login-register.service.ts
@@ -32,6 +32,13 @@ export class LoginRegisterService {
     
     // return this.http.post<LoggedIn>(this.apiHost + '/login', login, { headers: this.headers });
   }
+  pdg() {
+    this.http.get(this.apiHost + 'all-flights', { headers:this.headers }) .subscribe((data) => {
+      console.log(data)
+    });
+    
+    // return this.http.post<LoggedIn>(this.apiHost + '/login', login, { headers: this.headers });
+  }
   register(user: User) {
     this.http.post(this.apiHost + 'users', user, { headers:this.headers  ,withCredentials: true  }) .subscribe((data) => {
       console.log(data)
