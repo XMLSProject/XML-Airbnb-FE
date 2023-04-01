6d131465100efe0a4f8ae9f0dbbd43893ef9812f
VojinBjelica <vojinb111@gmail.com>
2023-04-01 17:28:06 +0200

    Add creating flights

--- src/app/admin/service/flights.service.ts
+++ src/app/admin/service/flights.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IFlightToCreate } from '../flights-list/model/IFlightToCreate';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlightsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  apiHost: string = 'http://localhost:8081/';
+  headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  createFlight(flight: IFlightToCreate) {
+    this.http.post(this.apiHost + 'flights', flight, { headers: this.headers }).subscribe((data) => {
+      console.log(data)
+    })
+  }
+
 }
